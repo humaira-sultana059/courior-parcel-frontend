c424fcda4267e094e16e2f560d3fc3740027f4b8
humaira-sultana059 <humairasultana059@gmail.com>
2025-12-30 15:46:44 +0600

    updated

--- src/app/customer/book-parcel/page.js
+++ src/app/customer/book-parcel/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { apiCall } from "@/utils/api";
 import Navigation from "@/components/common/Navigation";
